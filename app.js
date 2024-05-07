46102e4b87e6312e6a43546252fc11e6c987bd52
VanyaMark <markova.vanya@gmail.com>
2024-05-07 22:18:12 +0200

    used method .getComputedStyle to get the watermelon background-color from its css id

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 // Pika-pika suerte!
 
-// NO MODIFICAR 
+// NO MODIFICAR
 
-document.querySelector('#red').addEventListener('click', updateBgColor);
-document.querySelector('#blue').addEventListener('click', updateBgColor);
-document.querySelector('#green').addEventListener('click', updateBgColor);
+document.querySelector("#red").addEventListener("click", updateBgColor);
+document.querySelector("#blue").addEventListener("click", updateBgColor);
+document.querySelector("#green").addEventListener("click", updateBgColor);
+document.querySelector("#sangria").addEventListener("click", updateBgColor2);
 
-// MODIFICAR A PARTIR DE AQUÍ 
+// MODIFICAR A PARTIR DE AQUÍ
 
 function updateBgColor(event) {
+  let color = event.target.id;
+  if (color) {
+      console.log(`He clicado en el color ${color}`);
+      document.querySelector("img").style.backgroundColor = color;
+  } else {
+    const div = document.querySelector('div')
+    const divColor = window.getComputedStyle(div);
+    color = divColor.getPropertyValue('background-color');
+    console.log(color)
+    document.querySelector("img").style.backgroundColor = color;
+  }
+}
 
+function updateBgColor2(event) {
+    const div = document.querySelector('#sangria')
+    const divColor = window.getComputedStyle(div);
+    let color = divColor.getPropertyValue('background-color');
+    console.log(color)
+    document.querySelector("img").style.backgroundColor = color;
 }
